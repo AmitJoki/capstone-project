70e40c7a2a5d05ba8e63d0fa39f0d721befe28ae
Amit Joki <amitjoki@gmail.com>
2023-03-25 17:03:53 +0530

    reuse class name

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -1,3 +1,3 @@
 export default function ConfirmedBooking() {
-  return <article className='confirmed-booking'><h1>Your booking has been confirmed!</h1></article>;
+  return <article className='placeholder'><h1>Your booking has been confirmed!</h1></article>;
 }
